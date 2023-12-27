0445bc99b025e61bfc8f8dbe9a163226069becc7
keerthana5958v <127711144+keerthana5958v@users.noreply.github.com>
2023-12-27 16:46:53 +0530

    Add files via upload

--- SubstringPalindrome.java
+++ SubstringPalindrome.java
@@ -13,32 +13,15 @@ public class SubstringPalindrome {
         return (revStr.toString().equals(a));
     }
     void getAllSubstringPalindrome(String str){
-//        String str = "abbabcddcbab";
-//        int count = 0;
-        String s ="";
-        String s1 = "";
-        String[] arr;
-
         for(int i = 0 ; i < str.length()  ; i++){
             for(int j = 1 ; j + i < str.length(); j++){
-//                arr[i][j] = str.substring(j , (j+1) );
-//                if(i != 0 && j !=0) {
-                s = str.substring(i, i + j);
-                s1 += s + " ";
-//                }
-//                s += str.substring(i + j);
-//                s += "\n";
+                String s = str.substring(i, i + j);
+                if(isPalindrome(s)&& s.length()>1){
+                    System.out.println(s);
+                }
             }
         }
-        arr = s1.split(" ");
-//        SubstringPalindrome obj = new SubstringPalindrome();
 
-        for(int i = 0 ; i < arr.length ; i++){
-//            System.out.println(arr[i]);
-            if(isPalindrome(arr[i])&& arr[i].length()>1){
-                System.out.println(arr[i]);
-            }
-        }
     }
 
 }
